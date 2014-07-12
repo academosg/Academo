9dfd9e67c956cafa1a4f3c687a3c91c5f360a094
jeisern <jeisern246@hotmail.com>
2014-07-12 23:03:03 +0800

    Initial Upload of Project

---
+++ server/server.js
@@ -0,0 +1,362 @@
+/*
+- Complete all DO ME
+- Check how to use TURN server
+- Create a DB for logging ? 
+- Record timestamp
+- Disconnect user when user leave 
+- Client leave didnt destroy room - issue - remove feed not working 
+# Logging 
+# Fix package.json ( color and sql )
+# Fix EADDRINUSE
+# Fix ECONNCETREFUSED
+- 
+*/
+
+/*global console*/
+var yetify = require('yetify'),
+    config = require('getconfig'),
+    uuid = require('node-uuid'),
+    crypto = require('crypto'),
+    port = parseInt(process.env.PORT || config.server.port, 10),
+    io = require('socket.io').listen(port),
+    colors = require('color'),
+	mysql = require('mysql');
+
+
+if (config.logLevel) {
+    // https://github.com/Automattic/socket.io/wiki/Configuring-Socket.IO
+    io.set('log level', config.logLevel);
+}
+
+
+
+// Room Variables 
+var room = new Array();
+
+
+// Connect to DB
+var db_ready = true;
+/*
+var db_connection  = mysql.createPool(config.database);
+db_connection.getConnection(function(err, connection) {
+	if (err) 
+	{
+		custom_log('error','Database Failed ! - ' + err.code);
+		io.server.close();
+		return;
+	}
+	db_ready = true;
+	custom_log('info','Database connected !');
+});
+*/
+
+function custom_log(type, message) {
+	switch(type)
+	{
+		case 'error':
+		case 'ERROR':
+			console.log('ERROR'.red.bold, message);
+			break;
+		case 'info':
+		case 'INFO':
+			console.log('INFO'.green.bold,message);
+			break;
+		case 'debug':
+		case 'DEBUG':
+			console.log('DEBUG'.grey.bold,message);
+			break;
+		default :
+			console.log('UNKNOWN'.blue.bold,message);
+			break;
+	}
+}
+
+
+function describeRoom(name) {
+    var clients = io.sockets.clients(name);
+    var result = {
+        clients_resource: {},
+        clients_name: {}
+    };
+    clients.forEach(function (client) {
+        result.clients_resource[client.id] = client.resources;
+        result.clients_name[client.id] = client.name;
+    });
+    return result;
+}
+
+function safeCb(cb) {
+    if (typeof cb === 'function') {
+        return cb;
+    } else {
+        return function () {};
+    }
+}
+
+io.sockets.on('connection', function (client) {
+    
+	// init client
+	client.authenticate = false;
+	client.type = false;
+	
+	client.privilege = {
+		can_draw:true,
+		can_chat:true,
+		can_kick:false
+	};
+	
+	client.resources = {
+		screen: false,
+		video: true,
+		audio: false,
+	};
+
+    // pass a message 
+    client.on('message', function (details) {
+						
+		if (!details) return;			
+						
+		switch(details.to)
+		{
+			/*
+			*	Server 
+			*/
+			case 'server':
+				// Authentication 
+				if (details.type == 'authenticate')
+				{
+					/*
+					*  Authentication Code - DO ME
+					*/
+					var response, authenticate = true;
+					if (authenticate == true )
+					{
+						client.authenticate = true;
+						client.name = client.id;
+						client.type = 'tutor';
+						client.privilege.can_kick = true;
+						client.privilege.can_control_privilege = true;
+	
+						response = {
+							type: 'authenticate' , 
+							payload : {success : true, response : {client_type : client.type, client_name : client.name}}
+						};					
+						custom_log('info', client.name + ' has successfully authenticated');
+					}
+					else
+					{	
+						response = {
+							type: 'authenticate' , 
+							payload : {success : false, response : 'Invalid Session ID'}
+						};
+						custom_log('error', client.id + ' has failed to be authenticated');
+					}
+					client.emit('message', response);
+				}
+				else if (client.authenticate !== false)
+				{
+					// Control privilege
+					if (details.type == 'privilege')
+					{
+						if (client.privilege.can_control_privilege)
+						{
+							var otherClient = io.sockets.sockets[details.of];
+							if (!otherClient) return;
+							
+							switch(details.privilege)
+							{
+								case 'chat': 
+									otherClient.can_chat = details.can;
+									// emit something to that client - DO ME
+									break;
+								case 'whiteboard': 
+									otherClient.can_draw = details.can;
+									break;
+								default :
+									break;
+							}
+						}
+					}
+				}
+				break;
+				
+				
+			/*
+			*	Broadcasting 
+			*/
+			case 'broadcast': 
+				if (client.authenticate === false) return;
+				if ( ! client.room ) return;
+				
+				switch(details.type)
+				{
+					case 'whiteboard':
+						// Code for storing the drawing - DO ME
+						if ( ! client.privilege.can_draw ) return;
+						room[client.room].whiteboard.push(details);
+						client.broadcast.to(client.room).emit('message', details);
+						break;
+					case 'chat':
+						// Code for storing the chat - DO ME
+						if ( ! client.privilege.can_chat ) return;
+						room[client.room].chat.push(details);
+						client.broadcast.to(client.room).emit('message', details);
+						break;
+				}
+				
+				break;
+			
+			/*
+			*	Signalling 
+			*/
+			default:
+				if (client.authenticate === false) return;
+				var otherClient = io.sockets.sockets[details.to];
+				if (!otherClient) return;
+
+				details.from = client.id;
+				details.name = client.name;
+				otherClient.emit('message', details);	
+				
+				break;	
+		}
+		
+		return;
+		
+    });
+
+    client.on('shareScreen', function () {
+		if (client.authenticate === false) return;
+		
+        client.resources.screen = true;
+    });
+
+    client.on('unshareScreen', function (type) {
+		if (client.authenticate === false) return;
+		
+        client.resources.screen = false;
+        removeFeed('screen');
+    });
+
+    client.on('join', join);
+
+    function removeFeed(type) {
+        if (client.room) {
+            io.sockets.in(client.room).emit('remove', {
+                id: client.id,
+                type: type
+            });
+            if (!type) {
+				var client_room = client.room;
+                client.leave(client.room);
+                client.room = undefined;
+            }
+			
+			// remove the data if no one is in the room.
+			// maybe we can store the variables in server - DO ME
+			;
+			if ( ! io.sockets.clients(client_room).length )
+			{
+				custom_log('INFO', 'room ' + client_room + ' has been destroyed')
+				room[client_room] = null;
+				delete room[client_room];
+			}
+        }
+    }
+
+    function join(name, cb) {
+		if (client.authenticate === false) return;
+		
+        // sanity check
+        if (typeof name !== 'string') return;
+		
+        // leave any existing rooms
+        removeFeed();
+        safeCb(cb)(null, describeRoom(name));
+		
+        client.join(name);
+        client.room = name;
+		
+		// testing
+		// io.sockets.clients(client_room).length 
+		var names = ['','Jack','Samuel','Jeisern','Meepo']; 
+		var types = ['','Tutor','Student','Student','Student']; 
+		var i = io.sockets.clients(name).length ;
+		client.name = names[i];
+		client.type = types[i];
+		
+		
+		// send the room's existing data to the user
+		if (room[client.room])
+		{
+			custom_log('INFO', client.id + ' has joined room ' + client.room);
+			for(var i = 0; i < room[client.room].whiteboard.length ; i ++ )
+				client.emit('message', room[client.room].whiteboard[i]);
+			for(var i = 0; i < room[client.room].chat.length ; i ++ )
+				client.emit('message', room[client.room].chat[i]);			
+		}		
+		else
+		{
+			custom_log('INFO', client.id + ' has create room ' + client.room);
+			room[client.room] = {
+				whiteboard: [],
+				chat: [],
+			};
+		}
+				
+    }
+
+    // we don't want to pass "leave" directly because the
+    // event type string of "socket end" gets passed too.
+    client.on('disconnect', function () {
+		custom_log('debug', client.id + ' has disconnected from the room ' + client.room);
+		removeFeed();
+    });
+    client.on('leave', function () {
+		custom_log('debug', client.id + ' has left the room ' + client.room);
+        removeFeed();
+    });
+
+    client.on('create', function (name, cb) {
+		if (client.authenticate === false) return;
+		
+        if (arguments.length == 2) {
+            cb = (typeof cb == 'function') ? cb : function () {};
+            name = name || uuid();
+        } else {
+            cb = name;
+            name = uuid();
+        }
+		
+		
+        // check if exists
+        if (io.sockets.clients(name).length) {
+            safeCb(cb)('taken');
+        } else {
+            join(name);
+            safeCb(cb)(null, name);
+        }
+    });
+
+    // tell client about stun and turn servers and generate nonces
+    client.emit('stunservers', config.stunservers || []);
+
+    // create shared secret nonces for TURN authentication
+    // the process is described in draft-uberti-behave-turn-rest
+    var credentials = [];
+    config.turnservers.forEach(function (server) {
+        var hmac = crypto.createHmac('sha1', server.secret);
+        // default to 86400 seconds timeout unless specified
+        var username = Math.floor(new Date().getTime() / 1000) + (server.expiry || 86400) + "";
+        hmac.update(username);
+        credentials.push({
+            username: username,
+            credential: hmac.digest('base64'),
+            url: server.url
+        });
+    });
+    client.emit('turnservers', credentials);
+});
+
+if (config.uid) process.setuid(config.uid);
+console.log(yetify.logo() + ' -- signal master is running at: http://localhost:' + port);
