7446ea2ebaa89f4275ab38adf37cf7001a66bbf4
Savin <savin.varshney@gmail.com>
2015-07-20 07:05:44 +0800

    heroku production pg test

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,9 @@ ActiveRecord::Schema.define(version: 20150620044705) do
     t.datetime "updated_at"
   end
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entrepreneurs", force: true do |t|
     t.text     "name"
     t.text     "degree"
@@ -127,8 +130,8 @@ ActiveRecord::Schema.define(version: 20150620044705) do
     t.string   "role"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
